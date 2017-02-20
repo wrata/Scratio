0c771e3fcb506e53f0d9889b788fbbd5fe405794
Wiesław Rychlicki <wrata@poczta.onet.pl>
2017-02-20 17:16:00 +0100

    Update Scratio.ino

--- ArduinoCode/Scratio/Scratio.ino
+++ ArduinoCode/Scratio/Scratio.ino
@@ -1,6 +1,6 @@
 #include <Servo.h>
 
-#define AMAX 7   //A0-A6
+#define AMAX 8   //A0-A6, A7
 #define DIN_MAX 3  //2,3,4
 #define DOUT_MAX 3  //10,11,13
 #define AOUT_MAX 2  //5,6
@@ -18,7 +18,7 @@
 int din[DIN_MAX] = {2,3,4};
 int dout[DOUT_MAX] = {10,11,13};
 int aout[AOUT_MAX] = {5,6};
-int ain[AMAX] = {A0,A1,A2,A3,A4,A5,A6}; //A0-A6
+int ain[AMAX] = {A0,A1,A2,A3,A4,A5,A6,A7}; //A0-A6, A7
 int nloop;
 int pin;
 int value;
